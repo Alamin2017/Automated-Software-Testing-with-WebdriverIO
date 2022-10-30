b3d749c74ed79e1946c6708a2bf8a627df3c4d84
Al-Amin <alamincse12@gmail.com>
2022-10-30 16:16:13 +0600

    new added

--- pages/ExamplePage.js
+++ pages/ExamplePage.js
@@ -23,4 +23,43 @@ module.exports=new class ExamplePage{
     {
         return $("//h1[normalize-space()='Shopping Cart']")
     }
+    get promo_code()
+    {
+        return $("//a[normalize-space()='Have a promo code?']")
+    }
+    get promo_text_box()
+    {
+        return $("//input[@placeholder='Promo code']")
+    }
+    get promo_add_button()
+    {
+        return $("//span[normalize-space()='Add']")
+    }
+    get Mr_checkbox()
+    {
+        return $("//label[normalize-space()='Mr.']")
+    }
+    get first_name()
+    {
+        return $("//input[@name='firstname']")
+    }
+    get last_name()
+    {
+        return $("//input[@name='lastname']")
+    }
+    get email()
+    {
+        return $("//form[@id='customer-form']//input[@name='email']")
+    }
+    get terms_conditions()
+    {
+        return $("//input[@name='psgdpr']")
+    }
+    get continue_button1()
+    {
+        return $("//button[@data-link-action='register-new-customer']")
+    }
+
+
+
 }
--- test/specs/example.js
+++ test/specs/example.js
@@ -1,5 +1,5 @@
 const page=require('../../pages/ExamplePage');
-describe("My First test suite",()=>{
+describe("UK product website:->",()=>{
     it("End to End Test for Product",async()=>{
         
         await browser.url("http://teststore.automationtesting.co.uk/")
@@ -12,11 +12,39 @@ describe("My First test suite",()=>{
         await page.quantity_increase.click()
         await browser.pause(2000)
         await page.add_to_cart_button.click()
-        await browser.pause(5000)
+        await browser.pause(2000)
         await page.proceed_to_checkout_button.click()
-        await browser.pause(5000)
+        await browser.pause(2000)
         expect(await page.Title.getText()).toEqual("SHOPPING CART")
-        await browser.pause(5000)
+        await browser.pause(2000)
+        await page.promo_code.click()
+        await browser.pause(2000)
+        await page.promo_text_box.setValue("20OFF")
+        await browser.pause(2000)
+        await page.promo_add_button.click()
+        await browser.pause(2000)
+        await page.proceed_to_checkout_button.click()
+        await browser.pause(2000)
+        await page.Mr_checkbox.click()
+        await browser.pause(2000)
+        await page.first_name.setValue("John")
+        await browser.pause(2000)
+        await page.last_name.setValue("smith")
+        await browser.pause(2000)
+        await page.email.setValue("johnsmith@gmail.com")
+        await browser.pause(2000)
+        await page.terms_conditions.click()
+        await browser.pause(2000)
+        await page.continue_button1.click()
+        await browser.pause(2000)
+
+
+
+
+
+
+
+
 
     })
 
